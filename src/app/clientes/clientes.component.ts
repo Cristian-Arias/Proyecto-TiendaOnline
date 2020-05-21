50ce4b426ced3eb3ea744f021101268faecb0ee6
Cristian-Arias <cristian_an@corhuila.edu.co>
2020-05-21 00:11:53 -0500

    creación del frontem, con las siguientes opciones en menu: Home, Directivas, Clientes, Link, Dropdown y Disabled
    Co-Authored-By: Carolinaz011 <carolina_za@corhuila.edu.co>

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
@@ -7,18 +9,14 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
+  clientes: Cliente[];
 
-    {id: 1, nombre:'Carolina', apellido: 'Zuluaga', email: 'carolina_za@corhuila.edu.co', createAt: '2020-05-14' },
-    {id: 2, nombre:'Daniela', apellido: 'Neira', email: 'daniela_na@corhuila.edu.co', createAt: '2020-05-14' },
-    {id: 1, nombre:'Katherine', apellido: 'Hernandez', email: 'ingridhernandez_20182@corhuila.edu.co', createAt: '2020-05-14' },
-    {id: 1, nombre:'Cristian', apellido: 'Arias', email: 'cristian_an@corhuila.edu.co', createAt: '2020-05-14' }
+  constructor(private clienteService: ClienteService) { }
 
-  ];
-
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+  this.clienteService.getClientes().subscribe(
+    clientes => this.clientes = clientes
+   );
   }
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES : Cliente[] = [
+
+ {id: 1, nombre:'Carolina', apellido: 'Zuluaga', email: 'carolina_za@corhuila.edu.co', createAt: '2020-05-14' },
+ {id: 2, nombre:'Daniela', apellido: 'Neira', email: 'daniela_na@corhuila.edu.co', createAt: '2020-05-14' },
+ {id: 1, nombre:'Katherine', apellido: 'Hernandez', email: 'ingridhernandez_20182@corhuila.edu.co', createAt: '2020-05-14' },
+ {id: 1, nombre:'Cristian', apellido: 'Arias', email: 'cristian_an@corhuila.edu.co', createAt: '2020-05-14' }
+
+];
